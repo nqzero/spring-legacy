703ceb7418325f1058de0ed44a5058d08d98fb3e
lytles@takashi <srl.source@nqzero.com>
2018-01-19 16:11:07 -0500

    cleanup vestigal stuff left over from trying to figure this out

--- src/foobar/SpringLegacy.java
+++ src/foobar/SpringLegacy.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.event.ApplicationContextEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -37,20 +35,11 @@ public class SpringLegacy {
     public static class DummyObject {}
     
     public static void main(final String[] args) throws Exception {
-        // https://fraaargh.wordpress.com/2013/10/24/add-an-existing-beanobject-to-a-spring-applicationcontext-using-javaconfig/
-        // ApplicationContextAware
-        // https://stackoverflow.com/questions/9657961/best-practise-of-injecting-applicationcontext-in-spring3
         
         
         DummyObject obj = new DummyObject();
         DummyObject other = new DummyObject();
         SpringApplication app = new SpringApplication(SpringLegacy.class);
-        boolean stuff = false;
-        if (stuff)
-            SpringApplication.run(SpringLegacy.class, args);
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        ConfigurableApplicationContext c2 = null;
-        context.getBeanFactory().registerSingleton("myBean", obj);
         ApplicationListener<ApplicationContextEvent> lis = new ApplicationListener() {
             @Override
             public void onApplicationEvent(ApplicationEvent event) {
@@ -63,24 +52,21 @@ public class SpringLegacy {
                 }
             }
         };
-
-        // https://stackoverflow.com/questions/8686507/how-to-add-a-hook-to-the-application-context-initialization-event
-        // https://spring.io/blog/2015/02/11/better-application-events-in-spring-framework-4-2
-
-
-        // recent question asking exactly this:
-        // https://stackoverflow.com/questions/47911145/injecting-singletons-in-to-a-spring-boot-application-context
-
-        // legacy apps:
-        // http://www.captaindebug.com/2012/03/integrating-spring-into-legacy.html#.Wl2hL3WnHSU
-        
         app.setListeners(Arrays.asList(lis));
         app.run(args);
-//        app.setApplicationContextClass(context);
-//        context.register(SpringMatter.class);
-//        context.refresh();
-//        System.out.println(context.getBean("myBean"));
-//        new SpringApplicationBuilder(SpringMatter.class)
-//                        .properties("foo=bar").run(args);
+
+        
     }
 }
+
+
+// application events:
+// https://stackoverflow.com/questions/8686507/how-to-add-a-hook-to-the-application-context-initialization-event
+// https://spring.io/blog/2015/02/11/better-application-events-in-spring-framework-4-2
+
+
+// recent question asking exactly this:
+// https://stackoverflow.com/questions/47911145/injecting-singletons-in-to-a-spring-boot-application-context
+
+// legacy apps, ie registerSingleton:
+// http://www.captaindebug.com/2012/03/integrating-spring-into-legacy.html#.Wl2hL3WnHSU
