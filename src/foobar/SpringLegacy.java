01c49c543c3d4153e456028894e023202d59a605
lytles@takashi <srl.source@nqzero.com>
2018-01-19 16:40:33 -0500

    cleanup - minimal implementation

--- src/foobar/SpringLegacy.java
+++ src/foobar/SpringLegacy.java
@@ -20,55 +20,37 @@ import org.springframework.web.bind.annotation.RestController;
 @ResponseBody
 @SpringBootApplication
 public class SpringLegacy {
+    public SpringLegacy() {}
 
     @Lazy
     @Autowired
     @Qualifier("myBean")
-    DummyObject myBean;
+    Object myBean;
     
     @RequestMapping("/dir")
     public Object dir() {
         return "hello world " + myBean;
     }
 
-    public SpringLegacy() {}
-    public static class DummyObject {}
-    
     public static void main(final String[] args) throws Exception {
-        
-        
-        DummyObject obj = new DummyObject();
-        DummyObject other = new DummyObject();
+        Object obj = new Object();
         SpringApplication app = new SpringApplication(SpringLegacy.class);
         ApplicationListener<ApplicationContextEvent> lis = new ApplicationListener() {
             boolean first = true;
             @Override
             public void onApplicationEvent(ApplicationEvent event) {
                 if (first & event instanceof ContextRefreshedEvent) {
-                    ApplicationContext c3 = ((ContextRefreshedEvent) event).getApplicationContext();
-                    GenericApplicationContext c4 = (GenericApplicationContext) c3;
-                    c4.getBeanFactory().registerSingleton("myBean", obj);
-                    c4.getBeanFactory().registerSingleton("otherBean", other);
-                    System.out.println(c3);
+                    ((GenericApplicationContext) (((ContextRefreshedEvent) event).getApplicationContext())).
+                            getBeanFactory().registerSingleton("myBean", obj);
                     first = false;
                 }
             }
         };
         app.setListeners(Arrays.asList(lis));
         app.run(args);
-
-        
     }
 }
 
 
-// application events:
-// https://stackoverflow.com/questions/8686507/how-to-add-a-hook-to-the-application-context-initialization-event
-// https://spring.io/blog/2015/02/11/better-application-events-in-spring-framework-4-2
-
-
 // recent question asking exactly this:
 // https://stackoverflow.com/questions/47911145/injecting-singletons-in-to-a-spring-boot-application-context
-
-// legacy apps, ie registerSingleton:
-// http://www.captaindebug.com/2012/03/integrating-spring-into-legacy.html#.Wl2hL3WnHSU
